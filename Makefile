96a1fe939e0d323ee9aafcb5ceca22eece06f03f
ashn <me@ashn.dev>
2020-05-22 22:17:01 -0400

    Init

---
+++ Makefile
@@ -0,0 +1,25 @@
+.POSIX:
+.SUFFIXES:
+.PHONY: all clean format
+.SILENT: clean
+
+C99_DBG = -O0 -g
+C99_REL = -DNDEBUG
+
+CC = c99
+CFLAGS = $(C99_REL)
+OBJS = bfi.o
+TARGET = bfi
+
+$(TARGET): $(OBJS)
+	$(CC) -o $@ $(OBJS) $(CFLAGS)
+
+clean:
+	rm -f $(TARGET) $(OBJS)
+
+format:
+	clang-format -i *.c
+
+.SUFFIXES: .c .o
+.c.o:
+	$(CC) -o $@ $(CFLAGS) -c $<
---
+++ bfi.c
@@ -0,0 +1,219 @@
+#include <errno.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define VERSION "0.0"
+
+#define CELL_COUNT 30000
+uint8_t cells[CELL_COUNT] = {0};
+
+static void
+errorf(char const* fmt, ...);
+static void*
+xalloc(void* ptr, size_t size);
+static void
+xslurp(unsigned char** out_buf, size_t* out_buf_size, char const* path);
+
+static void
+usage(void);
+static void
+argcheck(int argc, char** argv);
+static int
+run(unsigned char const* source, size_t source_size);
+
+int
+main(int argc, char** argv)
+{
+    unsigned char* source = NULL;
+    size_t source_size = 0;
+
+    argcheck(argc, argv);
+    xslurp(&source, &source_size, argv[1]);
+    int const status = run(source, source_size);
+
+    free(source);
+    return status;
+}
+
+static void
+errorf(char const* fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    fputs("error: ", stderr);
+    vfprintf(stderr, fmt, args);
+    fputs("\n", stderr);
+    va_end(args);
+}
+
+static void*
+xalloc(void* ptr, size_t size)
+{
+    if (size == 0) {
+        free(ptr);
+        return NULL;
+    }
+    if ((ptr = realloc(ptr, size)) == NULL) {
+        errorf("Out of memory!");
+        exit(EXIT_FAILURE);
+    }
+    return ptr;
+}
+
+static void
+xslurp(unsigned char** out_buf, size_t* out_buf_size, char const* path)
+{
+    FILE* const stream = fopen(path, "rb");
+    if (stream == NULL) {
+        errorf("%s!", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned char* buf = NULL;
+    size_t size = 0;
+
+    int c;
+    while ((c = fgetc(stream)) != EOF) {
+        buf = xalloc(buf, size + 1);
+        buf[size++] = (unsigned char)c;
+    }
+    if (!feof(stream) || ferror(stream)) {
+        errorf("Failed to slurp file '%s'!", path);
+        exit(EXIT_FAILURE);
+    }
+
+    fclose(stream);
+    *out_buf = buf;
+    *out_buf_size = size;
+}
+
+static void
+usage(void)
+{
+    // clang-format off
+    puts(
+        "Usage: bfi FILE"                                          "\n"
+        "  -h, --help       Display usage information and exit."   "\n"
+        "      --version    Display version information and exit."
+    );
+    // clang-format on
+}
+
+static void
+argcheck(int argc, char** argv)
+{
+    if (argc == 1) {
+        usage();
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+            usage();
+            exit(EXIT_SUCCESS);
+        }
+        if (strcmp(argv[i], "--version") == 0) {
+            puts(VERSION);
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    if (argc > 2) {
+        errorf("More than one file provided!");
+        exit(EXIT_FAILURE);
+    }
+}
+
+static int
+run(unsigned char const* source, size_t source_size)
+{
+    size_t* const jumps = xalloc(NULL, source_size * sizeof(size_t));
+    size_t* const lines = xalloc(NULL, source_size * sizeof(size_t));
+    memset(jumps, 0x00, source_size * sizeof(size_t));
+    memset(lines, 0x00, source_size * sizeof(size_t));
+    size_t* const stack = xalloc(NULL, source_size * sizeof(size_t));
+    memset(stack, 0x00, source_size * sizeof(size_t));
+    size_t stack_count = 0;
+
+    size_t line = 1; // Current line in the source file.
+    for (size_t i = 0; i < source_size; ++i) {
+        lines[i] = line;
+        switch (source[i]) {
+        case '\n':
+            line += 1;
+            break;
+        case '[':
+            stack[stack_count++] = i;
+            break;
+        case ']':
+            if (stack_count == 0) {
+                errorf(
+                    "[line %zu] Unbalanced right square bracket ']' ...", line);
+                return EXIT_FAILURE;
+            }
+            stack_count -= 1;
+            jumps[stack[stack_count]] = i;
+            jumps[i] = stack[stack_count];
+            break;
+        }
+    }
+    for (size_t i = 0; i < stack_count; ++i) {
+        errorf(
+            "[line %zu] Unbalanced left square bracket '[' ...",
+            lines[stack[i]]);
+    }
+    if (stack_count != 0) {
+        return EXIT_FAILURE;
+    }
+
+    size_t cell_idx = 0;
+    for (size_t pc = 0; pc < source_size; ++pc) {
+        int c;
+        switch (source[pc]) {
+        case '+':
+            cells[cell_idx] += 1;
+            break;
+        case '-':
+            cells[cell_idx] -= 1;
+            break;
+        case '>':
+            if (cell_idx == (CELL_COUNT - 1)) {
+                errorf("[line %zu] '>' Causes cell out of bounds!", lines[pc]);
+                exit(EXIT_FAILURE);
+            }
+            cell_idx += 1;
+            break;
+        case '<':
+            if (cell_idx == 0) {
+                errorf("[line %zu] '<' Causes cell out of bounds!", lines[pc]);
+                exit(EXIT_FAILURE);
+            }
+            cell_idx -= 1;
+            break;
+        case '[':
+            if (cells[cell_idx] == 0) {
+                pc = jumps[pc];
+            }
+            break;
+        case ']':
+            pc = jumps[pc] - 1;
+            break;
+        case '.':
+            fputc(cells[cell_idx], stdout);
+            break;
+        case ',':
+            if ((c = fgetc(stdin)) != EOF) {
+                cells[cell_idx] = (uint8_t)c;
+            }
+            break;
+        }
+    }
+
+    free(jumps);
+    free(lines);
+    free(stack);
+    return EXIT_SUCCESS;
+}
